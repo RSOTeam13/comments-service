c5701e2ce93351b7e874c8d789c5c5e3d37069a0
Anze Mur <am3158@student.uni-lj.si>
2021-01-06 21:40:17 +0100

    test

--- api/src/main/java/si/fri/rso/albify/commentservice/api/v1/CommentServiceApplication.java
+++ api/src/main/java/si/fri/rso/albify/commentservice/api/v1/CommentServiceApplication.java
@@ -1,9 +1,14 @@
 package si.fri.rso.albify.commentservice.api.v1;
 
+import com.kumuluz.ee.cors.annotations.CrossOrigin;
+import com.kumuluz.ee.graphql.GraphQLApplication;
+import com.kumuluz.ee.graphql.annotations.GraphQLApplicationClass;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/v1")
-public class CommentServiceApplication extends Application {
+// @ApplicationPath("/v1")
+@GraphQLApplicationClass
+public class CommentServiceApplication extends GraphQLApplication {
 
 }
